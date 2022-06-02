27eaba93d67e3774922a5d9908039090ba6c7b4d
skinshy <yyshgk12@gmail.com>
2022-06-02 11:07:26 +0900

    update ci cd

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker stop newsncashfront
+docker rmi newsncash/front:latest
+git pull origin master
+docker build -t newsncash/front .
+docker run -p 38380:3000 —name newsncashfront -i -d —rm newsncash/front
+#docker logs -f front >> ./container_output_`date "+%Y%m%d"`.log 2>&1 &
+#tail -f ./container_output_`date "+%Y%m%d"`.log
