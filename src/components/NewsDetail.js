143741aff14f6b4f55157e42de1a9a649c6d51b2
anisia20 <anisia20@nate.com>
2022-07-03 12:29:54 +0900

    test

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Divider, Stack, Typography} from "@mui/material";
+import {Box, Divider, Stack, Typography, Link} from "@mui/material";
 import {styled} from "@mui/material/styles";
 import {createNews} from "../utils/news";
 
