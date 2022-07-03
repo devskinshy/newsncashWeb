35a3a95810e237d95be740d6281d9b36fe576efb
anisia20 <anisia20@nate.com>
2022-07-03 11:22:59 +0900

    #csm - modi Copyright - add provider link

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -83,9 +83,13 @@ const NewsDetail = ({content}) => {
         <Box dangerouslySetInnerHTML={{__html: createNews(body)}}/>
       </ContentBoxStyled>
       <LinkBoxStyled spacing={1}>
-        <Typography variant={'subtitle2'}>ⓒ {nm}</Typography>
-        <Typography variant={'button'}>언론사에서 보기 ></Typography>
-        <Typography variant={'button'}>네이버에서 보기 ></Typography>
+        <Typography variant={'subtitle2'}>Copyright ⓒ {nm}. All rights reserved. 무단 전재 및 재배포 금지</Typography>
+        <Typography variant={'button'}>
+          <Link href="#" underline="none" to={provider_link_page}>
+          언론사에서 보기 >
+          </Link>
+        </Typography>
+        {/* <Typography variant={'button'}>네이버에서 보기 ></Typography> */}
       </LinkBoxStyled>
     </RootStyled>
   );
